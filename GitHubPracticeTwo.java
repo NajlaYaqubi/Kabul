78c6177254bccf826081c8139a85a3db3a895daa
Najla <najla.yaqubi@tekschool.us>
2023-03-04 17:34:16 -0500

    my first practice

---
+++ GitHubPracticeTwo.java
@@ -0,0 +1,13 @@
+package githubperactic;
+
+public class GitHubPracticeTwo {
+	
+public int test (int a, int b) {
+	  
+		return  a*b;  
+		
+	  
+
+ }
+
+}
---
+++ GitpacticeNajlaBranch.java
@@ -0,0 +1,10 @@
+package githubperactic;
+
+public class GitpacticeNajlaBranch {
+	
+	public static void main(String []args) {
+		System.out.println("this is a message into Najla branch"); 
+		
+	}
+
+}
